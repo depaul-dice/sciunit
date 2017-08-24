1c5fff0b556fd4f75066d2ff391676891edea0e1
Zhihao Yuan <lichray@gmail.com>
2017-08-24 23:18:50 +0000

    chore: build and install binary dependencies
    Development mode (`pip install -e .`) doesn't do this step, meanwhile
    you may manually install a Debug version.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import os
+import subprocess
 from setuptools import setup
+from setuptools.command.build_py import build_py
 
 
 def localopen(fname):
     return open(os.path.join(os.path.dirname(__file__), fname))
+
+
+class BuildCommand(build_py):
+    def run(self):
+        subprocess.check_call(['cmake', '-DCMAKE_BUILD_TYPE=Release'])
+        subprocess.check_call(['make'])
+        build_py.run(self)
 
 
 setup(
@@ -14,6 +23,7 @@ setup(
     author='Zhihao Yuan',
     author_email='zhihao.yuan@depaul.edu',
     packages=["sciunit2"],
+    include_package_data=True,
     zip_safe=False,
     license='BSD',
     keywords=['sciunit'],
@@ -21,6 +31,7 @@ setup(
     long_description=localopen('README.rst').read(),
     install_requires=localopen('requirements.txt').readlines(),
     tests_require=localopen('test-requirements.txt').readlines(),
+    cmdclass={'build_py': BuildCommand},
     classifiers=[
         'Development Status :: 1 - Planning',
         'License :: OSI Approved :: BSD License',
