33f35c1032ebd4a00b800f6a93ec3b8a029131cc
Zhihao Yuan <zhihao.yuan@depaul.edu>
2017-11-15 10:44:00 +0000

    refactor: abstract out the 'commit' action

--- sciunit2/command/exec_.py
+++ sciunit2/command/exec_.py
@@ -1,17 +1,15 @@
 from __future__ import absolute_import
 
 from sciunit2.command import AbstractCommand
+from sciunit2.command.mixin import CommitMixin
 from sciunit2.exceptions import CommandLineError
-from sciunit2.util import quoted
-from sciunit2 import timestamp
 import sciunit2.core
 import sciunit2.workspace
 
 from getopt import getopt
-from humanfriendly import Spinner
 
 
-class ExecCommand(AbstractCommand):
+class ExecCommand(CommitMixin, AbstractCommand):
     name = 'exec'
 
     @property
@@ -32,13 +30,4 @@ class ExecCommand(AbstractCommand):
                 sciunit2.core.shell()
             else:
                 sciunit2.core.capture(args)
-            with Spinner('Committing') as sp:
-                sz = repo.checkin(rev, 'cde-package', sp)
-            return (repo.location,) + emgr.commit(sz)
-
-    def note(self, (p, rev, d)):
-        return "\n[%s %s] %s\n Date: %s\n" % (
-            sciunit2.workspace.project(p),
-            rev,
-            quoted(d.cmd),
-            timestamp.fmt_rfc2822(d.started))
+            return self.do_commit(rev, emgr, repo)
---
+++ sciunit2/command/mixin.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from sciunit2.util import quoted
+from sciunit2 import timestamp
+import sciunit2.workspace
+
+from humanfriendly import Spinner
+
+
+class CommitMixin(object):
+    def do_commit(self, rev, emgr, repo):
+        with Spinner('Committing') as sp:
+            sz = repo.checkin(rev, 'cde-package', sp)
+        return (repo.location,) + emgr.commit(sz)
+
+    def note(self, (p, rev, d)):
+        return "\n[%s %s] %s\n Date: %s\n" % (
+            sciunit2.workspace.project(p),
+            rev,
+            quoted(d.cmd),
+            timestamp.fmt_rfc2822(d.started))
