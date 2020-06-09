83f6e5ad83116211071ebd4b226e535b1d540559
raza <sra0nasir@gmail.com>
2020-06-09 09:39:59 -0500

    fix: updated path for Spinner and constructor call docs: added documentation

--- sciunit2/command/mixin.py
+++ sciunit2/command/mixin.py
@@ -1,22 +1,24 @@
-#Note: Converted
 from __future__ import absolute_import
 
 from sciunit2.util import quoted
-from sciunit2.cdelog import open
 from sciunit2 import timestamp
 import sciunit2.workspace
 
-from humanfriendly import Spinner
+from humanfriendly.terminal.spinners import Spinner
 
 
+# This class commits the most recent execution to
+# the de-duplication engine and the database.
+# A spinning animation is displayed as feedback
+# to the end-user during the entire time.
 class CommitMixin(object):
     def do_commit(self, pkgdir, rev, emgr, repo):
-        with Spinner('Committing') as sp:
+        with Spinner(label='Committing') as sp:
+            # adds the execution to de-duplication engine
             sz = repo.checkin(rev, pkgdir, sp)
+        # adds the execution to the database
         return (repo.location,) + emgr.commit(sz)
 
-    #def note(self, (p, rev, d)):
-    #def note(self, p, rev, d):
     def note(self, aList):
         return "\n[%s %s] %s\n Date: %s\n" % (
             sciunit2.workspace.project(aList[0]),
