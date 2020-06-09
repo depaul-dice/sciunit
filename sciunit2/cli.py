cbd414cc907add344cb418b4339d2e1846e9d1e2
raza <sra0nasir@gmail.com>
2020-06-09 09:47:53 -0500

    chore: updated EOF error reporting docs: added documentation

--- sciunit2/cli.py
+++ sciunit2/cli.py
@@ -16,24 +16,17 @@ from sciunit2.command.copy import CopyCommand
 from sciunit2.command.post_install import PostInstallCommand
 from sciunit2.command.diff import DiffCommand
 
-#Note: Converted
-
 import sys
 from getopt import getopt, GetoptError
 from io import StringIO
 import textwrap
 import pkg_resources
 import os
-#import pdb
 
-#__cmds__ = [CreateCommand, OpenCommand, ExecCommand, RepeatCommand,
-#            ListCommand, ShowCommand, GivenCommand, CommitCommand, RmCommand,
-#            SortCommand, PushCommand, CopyCommand, PostInstallCommand]
 __cmds__ = [CreateCommand, OpenCommand, ExecCommand, RepeatCommand,
             ListCommand, ShowCommand, GivenCommand, CommitCommand, RmCommand,
             SortCommand, PushCommand, CopyCommand, PostInstallCommand,
             DiffCommand]
-#__cmds__ = []
 
 
 def short_usage(out):
@@ -81,25 +74,18 @@ def main():
             err1(exc.strerror)
         sys.exit(1)
 
-#def hai(args):
-#    print (args)
-#    print ("Hai")
 
 def _main(args):
     optlist, args = getopt(args, '', ['help', 'version', 'root='])
-    #print optlist
-    #print args
-    #pdb.set_trace()
-
     if optlist:
         op, v = optlist[0]
         if op == '--help':
             short_usage(sys.stdout)
-            print
+            print()
             subcommand_usage(sys.stdout, [cls() for cls in __cmds__])
             return
         elif op == '--version':
-            print (pkg_resources.require("sciunit2")[0])
+            print(pkg_resources.require("sciunit2")[0])
             return
         elif op == '--root':  # pragma: no cover
             import sciunit2.workspace
@@ -124,7 +110,7 @@ def _main(args):
                     err2(cmd.name, exc)
                     sys.exit(1)
                 except EOFError:
-                    print
+                    err1("End of file error!")
                 break
         else:
             raise GetoptError('subcommand %r unrecognized' % args[0])
