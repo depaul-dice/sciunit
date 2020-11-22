392a0619c4b87b2b4062c2de3c8ca8be288f2545
Joshua Yang <joshuajy03@gmail.com>
2020-11-21 21:12:53 -0800

    feat: logging for remove implemented

--- sciunit2/command/rm.py
+++ sciunit2/command/rm.py
@@ -5,6 +5,7 @@ from sciunit2.exceptions import CommandLineError, MalformedExecutionId
 from sciunit2.util import quoted
 from sciunit2 import timestamp
 import sciunit2.workspace
+import sciunit2.logger
 import re
 
 from getopt import getopt
@@ -26,12 +27,14 @@ class RmCommand(AbstractCommand):
     def __to_id_range(revrange):
         r = re.match(r'^e([1-9]\d*)-([1-9]\d*)?$', revrange)
         if not r:
+            sciunit2.logger.runlog("error", "rm", "MalformedExecutionID", "rm.py")
             raise MalformedExecutionId
         return tuple(int(x) if x is not None else x for x in r.groups())
 
     def run(self, args):
         optlist, args = getopt(args, '')
         if len(args) != 1:
+            sciunit2.logger.runlog("error", "rm", "CommandLineError", "rm.py")
             raise CommandLineError
         emgr, repo = sciunit2.workspace.current()
         with emgr.exclusive():
