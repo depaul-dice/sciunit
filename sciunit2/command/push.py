7811b36702b0380da4582e5a1949c10085895ff5
Joshua Yang <joshuajy03@gmail.com>
2020-11-21 21:12:36 -0800

    feat: logging for push implemented

--- sciunit2/command/push.py
+++ sciunit2/command/push.py
@@ -29,6 +29,7 @@ class PushCommand(AbstractCommand):
         optlist, args = gnu_getopt(args, '', ['setup=', 'file='])
         setup = '--setup' in dict(optlist)
         if setup and not args or len(args) > 1 or len(optlist) > 1:
+            sciunit2.logger.runlog("error", "push", "CommandLineError", "push.py")
             raise CommandLineError
 
         emgr, repo = sciunit2.workspace.current()
@@ -42,11 +43,13 @@ class PushCommand(AbstractCommand):
             try:
                 srvname = article.service
             except NotFound as exc:
+                sciunit2.logger.runlog("error", "push", "CommandError: NotFound", "push.py")
                 raise CommandError(exc)
 
         try:
             srvcls = self.__srv[srvname]
         except KeyError:
+            sciunit2.logger.runlog("error", "push", "CommandError : unrecognized service %r" % srvname, "push.py")
             raise CommandError('unrecognized service %r' % srvname)
         else:
             srv = srvcls(sciunit2.credentials.for_(srvcls.name),
@@ -64,6 +67,7 @@ class PushCommand(AbstractCommand):
                 srv.push(article, fn)
                 sciunit2.sharing.article.save_recent(repo.location, article)
             except (NotAuthorized, NotFound) as exc:
+                sciunit2.logger.runlog("error", "push", "CommandError", "push.py")
                 raise CommandError(exc)
 
         datatuple = (article, sciunit2.workspace.at())
